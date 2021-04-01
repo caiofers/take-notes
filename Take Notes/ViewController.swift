1561e3a8086cf6a6f962becd2d8b031d00f30e87
Caio <caiovr13@gmail.com>
2021-03-31 22:07:48 -0300

    Associando View a ViewController

--- Take Notes/ViewController.swift
+++ Take Notes/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var listOfNotes: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfNotes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reuseCell = "reuseNoteCell"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseCell, for: indexPath)
+    
+        cell.textLabel?.text = listOfNotes[indexPath.row]
+        
+        return cell
+    }
+    
 
 }
 
